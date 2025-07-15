92a14d30cd22879dff408c905415e28ab2df1b07
Zulfiyya Mammadli <mammadli.zulfiyya07@gmail.com>
2025-07-15 22:13:48 +0400

    Fix task_2: correct dependencies order and complete setup

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
-// Şəkli import edirik
-import logoImage from '../assets/holberton-logo.jpg';
 
 let count = 0;
 
@@ -12,24 +10,12 @@ function updateCounter() {
 }
 
 $(document).ready(function() {
-  console.log('JavaScript started');
-  
-  // Logo elementini yaradırıq və şəkli dinamik set edirik
-  const logoDiv = $('<div id="logo"></div>');
-  logoDiv.css('background-image', `url(${logoImage})`);
-  $('body').prepend(logoDiv);
-  
-  console.log('Logo div added with image:', logoImage);
-  
-  // Digər elementləri əlavə edirik
+  $('body').prepend('<div id="logo"></div>');
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  
-  // Button və counter container
   $('body').append('<div class="button-container"></div>');
   $('.button-container').append('<button id="start-button">Click here to get started</button>');
   $('.button-container').append('<span id="count"></span>');
-  
   $('body').append('<p>Copyright - Holberton School</p>');
   
   $('#start-button').on('click', _.debounce(updateCounter, 500));
